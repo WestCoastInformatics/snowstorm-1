e3b0cd4e2db3072fc6e4c71d05f31551b0a711e9
RichardAWood <rickawood@gmail.com>
2022-12-11 22:45:13 -0800

    Revert "RT2-1505: reworking ECL to run across all branches."
    This reverts commit dc71a636b796a1c6b5cb1ec882fba44253455092.

--- src/main/java/org/snomed/snowstorm/core/data/services/MultiSearchService.java
+++ src/main/java/org/snomed/snowstorm/core/data/services/MultiSearchService.java
@@ -72,14 +72,9 @@ public class MultiSearchService implements CommitListener {
 
 		if (ecl != null) {
 			// ECL -> conceptIds
-			Set<Long> conceptIds = new HashSet<>();
-			MultiBranchCriteria branchesQuery = getBranchesQuery();
-			for(BranchCriteria branchCriteria : branchesQuery.getBranchCriteria()) {
-				Page<Long> page = eclQueryService.selectConceptIds(ecl, branchCriteria, true, null, null);
-				conceptIds.addAll(page.toSet());
-			}
-			final Page<Long> page = new PageImpl<>(new ArrayList<Long>(conceptIds));
-			
+			BranchCriteria branchCriteria = versionControlHelper.getBranchCriteria("MAIN");
+			//MultiBranchCriteria branchesQuery = getBranchesQuery();
+			Page<Long> page = eclQueryService.selectConceptIds(ecl, branchCriteria, true, null, null);
 			criteria.conceptIds(page.getContent());
 		}
 
